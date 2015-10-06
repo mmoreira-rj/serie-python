b69068b141385cc59d7af0e089b3f5e7ca39cef1
Marcio Moreira <marcio.moreira@unimedvr.com.br>
2015-10-06 11:07:44 -0300

    POST 01

---
+++ app.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+nome_aluno = raw_input("Informe o nome do Aluno: \n")
+data_nascimento = raw_input("Informe a Data de Nascimento do Aluno: \n")
+nome_mae = raw_input("Informe o nome da mãe do Aluno: \n")
+numero_cpf = raw_input("Informe o CPF Aluno: \n")
+endereco = raw_input("Informe o Endereco do Aluno: \n")
+
+print "----------------------"
+print "Informacoes do Aluno: "
+print "----------------------"
+print "Nome:" + nome_aluno
+print "DataNascimento: " + data_nascimento
+print "Mãe: " + nome_mae
+print "CPF: " + numero_cpf
+print "Endereço: " + endereco
+print "----------------------"
+		
